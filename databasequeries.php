363a06122f4c21a3c0df1578b33490da952a00cf
Dusan <masinacdt@gmail.com>
2019-02-10 15:59:33 +0100

    ok

---
+++ databasequeries.php
@@ -0,0 +1,43 @@
+<?php
+/* Inserting Into table (query and multi query)
+
+$connect=new mysqli("localhost","root","","firezend");
+
+$query="INSERT INTO Customers (Name,Address,email) VALUES('Rex', 'Drenovac 3',
+                                                           'rex@gmail.com');";
+$query.="INSERT INTO Customers (Name, Address, email) VALUES('Milan', 'Obilicev Venac 5',
+                                                            'mt@yahoo.com')";
+$connect->multi_query($query) or die("$connect->error");
+}
+$connect->close();
+*/
+
+/* Selecting from table
+$connect=new mysqli("localhost","root","","firezend") or die("$connect->error");
+$query=" SELECT * FROM Customers WHERE name='Dulen' ";
+$result=$connect->query($query) or die("$connect->error");
+$row=$result->fetch_assoc();
+$connect->close();
+print_r($row);
+*/
+
+//Prepared statements
+$connect=new mysqli("localhost","root","","firezend") or die($connect->error);
+
+$insert=$connect->prepare("INSERT INTO Customers (Name, Address, email) VALUES (?,?,?)");
+$insert->bind_param("sss",$name,$address,$email);
+
+$name="Iven";
+$address="Maksima Gorkog 4A";
+$email="iven@gmail.com";
+$insert->execute();
+
+$name="Borko";
+$address="Jovana Pasica 3";
+$email="bk2132@gmail.com";
+$insert->execute();
+
+$insert->close();
+$connect->close();
+
+?>
